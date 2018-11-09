8c2868a38ae4eafbdb29e73d00911223e661fd21
kanin99 <hattakul99@gmail.com>
2018-11-09 19:17:07 +0200

    loto

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1541702590,
+	"version": 1541783303,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -17,6 +17,9 @@
 		"images/sprite7-sheet0.png",
 		"images/sprite8-sheet0.png",
 		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite12-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
